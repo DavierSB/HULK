9c74abbc30d3e7b80a9195c590977c934847f629
chavely15k <chavely15k@gmail.com>
2024-02-06 22:04:06 -0500

    symbol_table_002

--- symbol_table.py
+++ symbol_table.py
@@ -1,40 +1,39 @@
 from abc import ABC
 
-class token:
-    def __init__(self, name, value) -> None:
-        self.name = name
-        self.value
-
 class symbol(ABC):
-    def __init__(self, id=None, type=None, value=None, priority=None, associativity=None) -> None:
+    def __init__(self, id=None, type=None, priority=None, associativity=None) -> None:
         self.type = type
         self.id = id
-        self.value = value
         self.priority = priority
         self.associativity = associativity
         self.hasNext = False    
 
+class token:
+    def __init__(self, symbol: symbol, value) -> None:
+        self.value = value
+        self.symbol = symbol
+
 symbol_table = {}
 
-symbol_table['('] = symbol('(', 'sep', None, 1 , None)
-symbol_table[')'] = symbol(')', 'sep', None, 1 , None)
-symbol_table['['] = symbol('[', 'sep', None, 1 , None)
-symbol_table[']'] = symbol(']', 'sep', None, 1 , None)
-symbol_table['{'] = symbol('{', 'sep', None, 1 , None)
-symbol_table['}'] = symbol('}', 'sep', None, 1 , None)
-symbol_table[';'] = symbol(';', 'sep', None, 1 , None)
+symbol_table['('] = symbol('(', 'sep', 1 , None)
+symbol_table[')'] = symbol(')', 'sep', 1 , None)
+symbol_table['['] = symbol('[', 'sep', 1 , None)
+symbol_table[']'] = symbol(']', 'sep', 1 , None)
+symbol_table['{'] = symbol('{', 'sep', 1 , None)
+symbol_table['}'] = symbol('}', 'sep', 1 , None)
+symbol_table[';'] = symbol(';', 'sep', 1 , None)
 
-symbol_table['+'] = symbol('+', 'opp', None, 20, 'left')
-symbol_table['-'] = symbol('-', 'opp', None, 20, 'left')
-symbol_table['*'] = symbol('*', 'opp', None, 10, 'left')
-symbol_table['/'] = symbol('/', 'opp', None, 10, 'left')
-symbol_table['^'] = symbol('^', 'opp', None, 5 , 'left')
+symbol_table['+'] = symbol('+', 'opp', 20, 'left')
+symbol_table['-'] = symbol('-', 'opp', 20, 'left')
+symbol_table['*'] = symbol('*', 'opp', 10, 'left')
+symbol_table['/'] = symbol('/', 'opp', 10, 'left')
+symbol_table['^'] = symbol('^', 'opp', 5 , 'left')
 
-symbol_table['='] = symbol('=', 'opp', None, 30, None)
-symbol_table['<'] = symbol('<', 'opp', None, 25, None)
-symbol_table['>'] = symbol('>', 'opp', None, 25, None)
+symbol_table['='] = symbol('=', 'opp', 30, None)
+symbol_table['<'] = symbol('<', 'opp', 25, None)
+symbol_table['>'] = symbol('>', 'opp', 25, None)
 
-symbol_table[':='] = symbol(':=', 'opp', None, 30, None)
-symbol_table['<='] = symbol('<=', 'opp', None, 25, None)
-symbol_table['>='] = symbol('>=', 'opp', None, 25, None)
-symbol_table['=='] = symbol('==', 'opp', None, 25, None)
+symbol_table[':='] = symbol(':=', 'opp', 30, None)
+symbol_table['<='] = symbol('<=', 'opp', 25, None)
+symbol_table['>='] = symbol('>=', 'opp', 25, None)
+symbol_table['=='] = symbol('==', 'opp', 25, None)
