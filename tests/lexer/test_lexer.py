ef328c874dffa57aba3fda4de271c86337b99d16
chavely15k <chavely15k@gmail.com>
2024-02-16 14:59:55 -0500

    test

--- tests/lexer/test_lexer.py
+++ tests/lexer/test_lexer.py
@@ -1,9 +1,9 @@
 import unittest
 
-
 import sys
-sys.path.insert(0, '//home/chavely/Documents/start up/compiler/test 1/hulk test/lexer')
+import os
 
+sys.path.insert(0, os.getcwd() + '/lexer')
 from get_next_token import *
 
 class LexerTest(unittest.TestCase):
